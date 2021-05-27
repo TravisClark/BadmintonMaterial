8110cd0ed6d0797b054b7ccd6db73827e5a2c1c8
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2021-05-27 22:25:36 +0700

    update

--- WebApplication2/Controllers/SanPhamController.cs
+++ WebApplication2/Controllers/SanPhamController.cs
@@ -13,7 +13,7 @@ using Microsoft.AspNet.Identity;
 
 namespace WebApplication2.Controllers
 {
-    //[Authorize(Roles = "Admim")]
+    [Authorize(Roles = "Admim")]
     public class SanPhamController : Controller
     {
         
