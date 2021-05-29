f5839d6aacbad936d65be75a1a6f6bcf3ba6494b
197PM21988 - NGUYỄN HỒNG THỊNH - K25T-PM1 <thinh.197pm21988@vanlanguni.vn>
2021-05-29 21:27:17 +0700

    DatHang

---
+++ WebApplication2/Controllers/HoaDonKhachHangController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    
+    public class HoaDonKhachHangController : Controller
+    {
+        private List<ChiTietGioHang> cart = Session["cart"] as List<ChiTietGioHang>;
+        // GET: HoaDonKhachHang
+        public ActionResult Index()
+        {
+            var Session = System.Web.HttpContext.Current.Session;
+            cart = Session["cart"] as List<ChiTietGioHang>;
+            return View();
+        }
+
+        
+    }
+    
+}
