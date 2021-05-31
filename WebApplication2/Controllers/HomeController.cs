c8365e3327b03973cd4b19a115729e9ac0a48ceb
197PM09434 - Trần Thành Đạt - K25T-PM <dat.197pm09434@vanlanguni.vn>
2021-05-31 16:12:08 +0700

    fix index

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication2.Models;
+using Microsoft.AspNet.Identity;
+
 
 namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
+        public CT25Team17Entities db = new CT25Team17Entities();
         public ActionResult Index()
         {
-            return View();
+            var sp = db.SanPhams.ToList().Take(4);
+            ViewBag.hinh = ".png";
+
+
+            return View(sp);
         }
 
         public ActionResult About()
--- WebApplication2/Controllers/TrangChuController.cs
+++ WebApplication2/Controllers/TrangChuController.cs
@@ -1,27 +1,31 @@
-﻿using WebApplication2.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Transactions;
+using WebApplication2.Models;
 using Microsoft.AspNet.Identity;
+
 
 namespace WebApplication2.Controllers
 {
     public class TrangChuController : Controller
     {
         // GET: TrangChu
-
+        public CT25Team17Entities db = new CT25Team17Entities();
         public ActionResult Index()
         {
-            return View();
+            var sp = db.SanPhams.ToList().Take(4);
+            ViewBag.hinh = ".png";
+
+           
+            return View(sp);
         }
-        public ActionResult test()
-        {
-
-
-
-            return View();
-        }
+        
     }
 }
