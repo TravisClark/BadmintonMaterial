73086fd151d69397d86bfa9cea606ae88a64aa76
197PM33529 - NGUYỄN MINH PHÚ - K25T-PM1 <phu.197pm33529@vanlanguni.vn>
2021-06-01 12:43:46 +0700

    Thêm giao diện Admin

---
+++ WebApplication2/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Transactions;
+using WebApplication2.Models;
+using Microsoft.AspNet.Identity;
+
+
+namespace WebApplication2.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public CT25Team17Entities db = new CT25Team17Entities();
+        public ActionResult Index()
+        {        
+            return View();
+        }
+        
+    }
+}
